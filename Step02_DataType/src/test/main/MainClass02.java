ac11b6e75e9c122c1bed77984d73057caa129059
seungjin <nexo1@naver.com>
2017-11-14 10:19:37 +0900

    MainClass03.java까지 작성했습니다.

---
+++ Step02_DataType/src/test/main/MainClass02.java
@@ -0,0 +1,26 @@
+package test.main;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		
+	
+	//국어점수
+	int kor=95;
+	//영어점수
+	int eng=100;
+	//국어 영어 점수의 평균을 구해서 출력하고 싶다면?
+	
+	//두점수의 합
+	int sum=kor+eng;
+	/*
+	 *    점수끼리 연산하면 결과는 정수
+	 *    정수와 실수와 연산하면 결과는 실수 
+	 */
+	
+	//평균
+	double ave=sum/2.0;
+	
+	System.out.println("평균:"+ave);
+
+    }
+}
